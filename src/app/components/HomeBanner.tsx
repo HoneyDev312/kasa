28c54e3476e543af1700863db574aaea8c98fd3e
HoneyDev312 <honeydev312@gmail.com>
2026-05-26 02:11:16 +0200

    responsive homepage

--- src/app/components/HomeBanner.tsx
+++ src/app/components/HomeBanner.tsx
@@ -4,11 +4,11 @@ import styles from "./HomeBanner.module.css";
 
 export function HomeBanner() {
   return (
-    <section className={styles.hero} aria-labelledby="home-title">
-      <Typography as="h1" className={styles.title} id="home-title">
+    <section className={styles.container} aria-labelledby="home-title">
+      <Typography as="h1" className={styles.title} variant="h1">
         Chez vous, partout et ailleurs
       </Typography>
-      <Typography className={styles.intro} variant="regular">
+      <Typography className={styles.intro} variant="regular" color="dark">
         Avec Kasa, vivez des séjours uniques dans des hébergements chaleureux,
         sélectionnés avec soin par nos hôtes.
       </Typography>
